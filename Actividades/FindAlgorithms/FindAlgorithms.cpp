8c2effd3aba7a71208d1b9c6f8e081c629cb01c9
edan11v <a014119126@itesm.mx>
2022-08-30 17:33:20 -0500

    Subidas

---
+++ Actividades/FindAlgorithms/FindAlgorithms.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> list = {5, 8, 20, 4, 100, 15};
+
+    
+    
+    system("pause");
+    return 0;
+}
--- Actividades/Recursive/act1_1_2.cpp
+++ Actividades/Recursive/act1_1_2.cpp
@@ -74,7 +74,7 @@ x = 1 -> x
 x = 2 -> x * x
 x = 3 -> x * x * x
 x = 4 -> x * x * x * x
-/*
+
 
 int main()
 {
