cf66c04b74c870027108bc77a632daf38455fe5d
edan11v <a014119126@itesm.mx>
2022-11-08 15:31:47 -0600

    act

---
+++ ADTs/07-Graph/Graph.h
@@ -0,0 +1,75 @@
+#ifndef Graph_h
+#define Graph_h
+
+#include "Vertex.h"
+
+template<class T>
+class Graph {
+private:
+    vector< vector< Vertex<T> > > graph;
+    vector<T> vertices;
+    int size;
+    int findVertex(T vertex);
+public:
+    Graph();
+    Graph(vector<T> vertices);
+    Graph(vector<T> vertices, vector< Vertex<T> > edges);
+    void print();
+};
+
+// Constructor por default
+template<class T>
+Graph<T>::Graph() {
+    size = 0;
+}
+
+// Constructor con lista de vertices
+template<class T>
+Graph<T>::Graph(vector<T> vertices) {
+    this->vertices = vertices;
+    size = vertices.size();
+    // Cambiamos el tamaño de la matriz graph para almacenar todos los vertices
+    graph.resize(size);
+}
+
+template<class T>
+int Graph <T>::findVertex(T vertex) {
+    typename vector<int>::iterator it = find(vertices.begin(), vertices.end(), vertex);
+    if (it != vertices.end()) {
+        return it - vertices.begin();
+    } else {
+        return -1;
+    }     
+}
+
+template<class T>
+Graph<T>::Graph(vector<T> vertices, vector< Vertex<T> > edges) {
+    this->vertices = vertices;
+    size = vertices.size();
+    // Cambiamos el tamaño de la matriz graph para almacenar todos los vertices
+    graph.resize(size);
+    // iteramos en la lista de arcos para agregar cada arco
+    for (auto edge : edges) {
+        // buscar el indice del arco origen en la lista de vértices
+        int sourceIndex = findVertex(edge.source);
+        if (sourceIndex == -1) {
+            throw out_of_range("El vértice no se encuentra en el grafo");
+            return;
+        }
+        // agregamos el vertex en la poisición del sourceIndex en el grafo
+        graph[sourceIndex].push_back(edge);
+    }
+}
+
+template<class T>
+void Graph<T>::print() {
+    for (int i=0; i<size; i++) {
+        cout << vertices[i] << " ";
+        for (auto vertex : graph[i]) {
+            cout << vertex.target << "," << vertex.weight << " ";
+        }
+        cout << endl;
+    }
+}
+
+#endif
